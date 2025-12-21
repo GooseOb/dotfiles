35a9dcf69263fa089085c43bb1fb415de0a81dd8
GooseOb <gooose.ob@gmail.com>
2025-12-22 00:56:57 +0100

    add emacs

---
+++ emacs/.config/emacs/init.el
@@ -0,0 +1,21 @@
+(org-babel-load-file
+ (expand-file-name
+  "config.org"
+  user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(tango-dark))
+ '(package-selected-packages '(aggressive-indent-mode lsp-biome transient-cycles))
+ '(package-vc-selected-packages
+   '((aggressive-indent-mode :url
+			     "https://github.com/Malabarba/aggressive-indent-mode")
+     (lsp-biome :url "https://github.com/cxa/lsp-biome"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
