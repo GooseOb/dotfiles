3a24f0b91763a16f94852ab61bf62b9063a29d60
GooseOb <gooose.ob@gmail.com>
2025-12-28 16:40:00 +0100

    fix svelte mode & change theme

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -7,7 +7,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(tango-dark))
+ '(custom-enabled-themes '(gruber-darker))
+ '(custom-safe-themes
+   '("e27c9668d7eddf75373fa6b07475ae2d6892185f07ebed037eedf783318761d7"
+     "daa27dcbe26a280a9425ee90dc7458d85bd540482b93e9fa94d4f43327128077"
+     "d2ab3d4f005a9ad4fb789a8f65606c72f30ce9d281a9e42da55f7f4b9ef5bfc6"
+     "c20728f5c0cb50972b50c929b004a7496d3f2e2ded387bf870f89da25793bb44"
+     default))
  '(package-selected-packages '(aggressive-indent-mode lsp-biome transient-cycles))
  '(package-vc-selected-packages
    '((aggressive-indent-mode :url
